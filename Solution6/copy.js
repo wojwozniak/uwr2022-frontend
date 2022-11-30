ab2e7da953f87ff599ef47add923642585d59238
Wojtek <pulchnymamut@gmail.com>
2022-11-30 18:49:48 +0100

    tasks up to 8 finished

---
+++ Solution6/copy.js
@@ -0,0 +1,63 @@
+const cloneDeep = (input) => {
+  input = JSON.parse(JSON.stringify(input));
+  console.log(input.dates);
+  for(let i = 0; i<input.dates.length; i++) {
+    input[dates][i] = new Date(input[dates][i]);
+  }
+  return input;
+}
+
+const user = {
+  name: "Alice",
+  surname: "Young",
+  dates: {
+    birthDate: new Date(1995, 10, 28)
+  },
+  contact: {
+    phone: '111-111-111',
+    address: {
+      city: "London",
+    }
+  }
+};
+
+const userCp = cloneDeep(user);
+
+user.name = "John";
+user.surname = "White";
+user.dates.birthDate = new Date(1999, 11, 11);
+user.contact.phone = '222-222-222';
+user.contact.address.city = 'New York'
+
+console.log(user);
+/*
+{ 
+  name: 'John',
+  surname: 'White',
+  dates: {
+    birthDate: Sat Dec 11 1999 00:00:00 GMT+0100 (czas środkowoeuropejski standardowy)
+  },
+  contact: { 
+    phone: '222-222-222', 
+    address: { 
+      city: 'New York' 
+    } 
+  } 
+}
+*/
+console.log(userCp);
+/*
+{ 
+  name: 'Alice',
+  surname: 'Young',
+  dates: {
+    birthDate: Tue Nov 28 1995 00:00:00 GMT+0100 (czas środkowoeuropejski standardowy)
+  },
+  contact: { 
+    phone: '111-111-111', 
+    address: { 
+      city: 'London' 
+    } 
+  } 
+} 
+*/
