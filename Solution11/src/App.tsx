a50e8ed7e9a6bd28cec0c610be947c4ebd044081
Wojtek <pulchnymamut@gmail.com>
2023-01-16 12:24:37 +0100

    cleanup, copy css+html from t7

--- Solution11/src/App.tsx
+++ Solution11/src/App.tsx
@@ -1,33 +1,24 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <>
+      <h1>To Do List</h1>
+      <main id="wrapper">
+          <div id="container"></div>
+          <div id="control-panel">
+              <label id="control-panel__label" htmlFor="control-panel__controls">Add a new task to the list</label>
+              <div id="control-panel__controls">
+                  <textarea id="controls__textarea" placeholder="Write new task here:"></textarea>
+                  <button id="controls__button">+</button>
+              </div>
+          </div>
+      </main>
+      <p id="container__status"></p>
+      <button id="delete-all">Delete all tasks</button>
+    </>
   )
 }
 
