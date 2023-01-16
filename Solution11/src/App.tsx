da22b97b8fc8e9c886502760a9af7430cf3f5fd0
Wojtek <pulchnymamut@gmail.com>
2023-01-16 13:18:29 +0100

    adding tasks works!

--- Solution11/src/App.tsx
+++ Solution11/src/App.tsx
@@ -1,22 +1,91 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 
 function App() {
-  
+  const [containerStatus, updateContainerStatus] = useState(0);
+
+  interface task {
+    id: string,
+    text: string,
+    done: boolean
+  }
+
+  interface taskList {
+    tasks?: Array<task>
+  }
+
+  const [taskList, updateTaskList] = useState([
+  {
+    id: "2132",
+    text: "Example task",
+    done: false
+  },
+  {
+    id: "2137",
+    text: "Example task2",
+    done: true
+  },
+  ]);
+  const [currentText, updateCurrentText] = useState("");
+
+  const textarea = useRef() as React.MutableRefObject<HTMLTextAreaElement>;
+
+  useEffect(() => {
+    updateContainerStatus(taskList.length);
+  }, [taskList])
+
+  const handleTextUpdate = (event:React.ChangeEvent<HTMLTextAreaElement>) => {
+    updateCurrentText(event.target.value);
+  }
+
+  const handleClick = () => {
+
+    if(currentText != "") {
+      let id = new Date().getTime().toString();
+      let task:task = {
+        id: id,
+        text: currentText,
+        done: false
+      };
+      updateTaskList([...taskList, task]);
+      updateCurrentText("");
+    } else {
+      textarea.current.placeholder="You need to write your task here!";
+      textarea.current.style.border="1px solid red";
+      setTimeout(()=> {
+        textarea.current.placeholder="Write new task here:";
+        textarea.current.style.border="1px solid gray";
+      }, 2000)
+    }
+  }
 
   return (
     <>
       <h1>To Do List</h1>
       <main id="wrapper">
-          <div id="container"></div>
+          <div id="container">
+            {
+              taskList.map((task) => {
+                return <div id={task.id} className = 'task'>
+                  <p style={{textDecoration: task.done ? "line-through" : "none"}}>{task.text}</p>
+                  <div className="task__btns">
+                    <input className="task__check" type="checkbox"/>
+                    <button className="task__button">X</button>
+                  </div>
+                </div>
+              })
+            }
+          </div>
           <div id="control-panel">
               <label id="control-panel__label" htmlFor="control-panel__controls">Add a new task to the list</label>
               <div id="control-panel__controls">
-                  <textarea id="controls__textarea" placeholder="Write new task here:"></textarea>
-                  <button id="controls__button">+</button>
+                  <textarea id="controls__textarea" placeholder="Write new task here:" value={currentText} onChange={handleTextUpdate} ref={textarea}/>
+                  <button onClick={handleClick} id="controls__button">+</button>
               </div>
           </div>
       </main>
-      <p id="container__status"></p>
+      <p id="container__status">
+        You currently have {containerStatus} tasks.
+      </p>
       <button id="delete-all">Delete all tasks</button>
     </>
   )
