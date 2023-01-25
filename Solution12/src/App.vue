b977beafe351e9b6d317c72bad89b93ea9dd26c9
Wojtek <pulchnymamut@gmail.com>
2023-01-25 15:43:38 +0100

    deletion working

--- Solution12/src/App.vue
+++ Solution12/src/App.vue
@@ -1,56 +1,8 @@
 <script setup lang="ts">
-
-interface task {
-    id: string,
-    text: string,
-    done: boolean
-  }
-
-let taskString = localStorage.getItem("tasks");
-if(!taskString) {
-    taskString = ""
-}
-
-const initialState = JSON.parse(taskString);
-
-const tasks:task[] = initialState;
-
-const count = tasks.length;
-
-console.log(tasks);
-
+import Main from '../components/Main.vue'
 </script>
 
 <template>
   <h1>To Do List</h1>
-  <main id="wrapper">
-      <div id="container"> 
-        <div 
-        v-for="(task) in tasks" 
-        v-bind:id="task.id" 
-        class="task"
-        >
-          <p>{{task.text}}</p>
-          <div class="task__btns">
-            <input class="task__check" type="checkbox"/>
-            <button class="task__button">X</button>
-          </div>
-        </div>
-      </div>
-      <div id="control-panel">
-          <label id="control-panel__label" htmlFor="control-panel__controls">
-            Add a new task to the list
-          </label>
-          <div id="control-panel__controls">
-            <textarea 
-            id="controls__textarea" placeholder="Write new task here:" autoFocus
-            />
-            <button id="controls__button">+</button>
-          </div>
-      </div>
-  </main>
-  <p id="container__status">
-    You currently have {{count}} tasks.
-  </p>
-  <button id="delete-all">Delete all tasks</button>
+  <Main />
 </template>
